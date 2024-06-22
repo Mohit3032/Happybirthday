efac6fab84e8af10c1ee97a3934b4939c5983b98
Mohit <mohithundlani123@gmail.com>
2024-06-22 23:57:46 +0530

    hbd

---
+++ index.js
@@ -0,0 +1,10 @@
+const title = document.getElementById("title");
+const desc = document.getElementById("desc")
+const sname = document.getElementById("name")
+
+
+
+title.innerText = "To the girl who deserves more<3";
+
+desc.innerText = "Happy Birthday! 🎉 Hanin. May this year brings you endless joy, unforgettable moments, and the fulfillment of all your dreams. You deserve nothing but the best, today and always.";
+
